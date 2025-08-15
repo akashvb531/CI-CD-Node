8741ec89431868cb11d55ed812d1b4c856767fa3
akashvb531 <vbakash531@gmail.com>
2025-08-15 15:41:41 +0530

    add

--- scripts/after_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "After install - cleaning up old resources if any"
-# Example: delete old pods if needed (optional)
-# kubectl delete pods -l app=brain-tasks-app --namespace default
--- scripts/before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Before install - checking kubectl access"
-kubectl get nodes
-if [ $? -ne 0 ]; then
-  echo "kubectl access failed"
-  exit 1
-fi
-echo "kubectl access verified"
